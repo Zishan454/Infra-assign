81f7a1cf184307b7640123b8028f8daeae5549d4
Zishan454 <zeecloud6@google.com>
2022-02-11 16:27:07 +0530

    solved

---
+++ Solution/gencsv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RANDOM=$$
+num=0
+
+if [ -f "inputFile" ]; then
+  rm -rf inputFile
+fi
+
+while  [[ num -le 10 ]]
+ do
+   echo $num, $RANDOM >> inputFile
+   ((num= num + 1 ))
+ done 
